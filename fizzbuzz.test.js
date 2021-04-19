94bb007068496bb8b4a2e03b6bf4b1f69ebbe3f8
roberto <robpalacios1@hotmail.com>
2021-04-19 13:00:59 -0500

    add a new test when the argument is not a number

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,6 +1,12 @@
 const fizzbuzz = require('./fizzbuzz');
 
 describe('fizzbuzz', () => {
+  test('should print an error message if the argument is not a number', () => {
+    const expected = 'Error: the argument must be a number';
+    const result = fizzbuzz('16');
+    expect(expected).toBe(result);
+  })
+
   test('should print 1 if they receive 1', () => {
     const expected = 1;
     const result = fizzbuzz(1);
@@ -14,20 +20,26 @@ describe('fizzbuzz', () => {
   })
 
   test('should print fizz if the receive a multiple of 3', () => {
-    const expexted = 'fizz';
+    const expected = 'fizz';
     const result = fizzbuzz(6);
-    expect(expexted).toBe(result);
+    expect(expected).toBe(result);
   })
 
-  test('should print buzz if they receive  5', () => {
+  test('should print buzz if they receive 5', () => {
     const expected = 'buzz';
     const result = fizzbuzz(5);
     expect(expected).toBe(result);
   })
 
-  test('should print buzz if they receive multiple of  5', () => {
+  test('should print buzz if they receive multiple of 5', () => {
     const expected = 'buzz';
     const result = fizzbuzz(10);
+    expect(expected).toBe(result);
+  })
+
+  test('should print buzz if they receive multiple of 3 and 5', () => {
+    const expected = 'fizzbuzz';
+    const result = fizzbuzz(15);
     expect(expected).toBe(result);
   })
 });
