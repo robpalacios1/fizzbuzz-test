49c040c0e9c62ea526f5117f35cd64811ea3c34f
roberto <robpalacios1@hotmail.com>
2021-04-19 13:00:26 -0500

    refactor the code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,35 @@
 function fizzbuzz(num) {
-  if(num % 3 === 0) {
+  if(typeof num !== 'number') {
+    return "Error: the argument must be a number";
+  }
+
+  const divisible = (divisor, num) => num % divisor === 0;
+
+  if(num === 0) {
+    return 0;
+  }
+
+  if (divisible(3, num) && (divisible(5, num))){
+    return 'fizzbuzz'
+  }
+
+
+  if(divisible(3, num)) {
     return 'fizz';
   }
-  if(num % 5 === 0) {
+
+  if (divisible(5, num)) {
     return 'buzz';
   }
+  
   return num;
 }
+
+function print(num) {
+  for(let i = 0; i <= num; i++) {
+    console.log(`${i} : ${fizzbuzz(i)}`)
+  }
+}
+print(100);
 
 module.exports = fizzbuzz;
